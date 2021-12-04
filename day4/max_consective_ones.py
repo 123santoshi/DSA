aed4bd09123bc77ed6cd3473729327211a0136bc
santoshi mani kumari <70144325+123santoshi@users.noreply.github.com>
2021-12-04 09:21:30 +0530

    Add files via upload

--- day4/max_consective_ones.py
+++ day4/max_consective_ones.py
@@ -1,1 +1,16 @@
-
+l=[1,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0]
+s=""
+for i in l:
+    s+=str(i)
+print(s)
+l2=[]
+s1=s.split("1")
+l2.append(s1)
+print(s1)
+m=0
+for i in s1:
+    l3=len(i)
+    if(l3>m):
+        m=l3
+print(m)
+    
